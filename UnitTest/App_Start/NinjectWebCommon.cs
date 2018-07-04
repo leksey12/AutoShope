37e87bf9bf59bc2efa1a7f1ae82b7ef23e474ed3
Aleksey <alekseyarmany73@yandex.ru>
2018-07-04 23:37:16 +0400

    Зановово переустанавливал пакеты NutGet

--- UnitTest/App_Start/NinjectWebCommon.cs
+++ UnitTest/App_Start/NinjectWebCommon.cs
@@ -1,7 +1,7 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(WebUl.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(WebUl.App_Start.NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(UnitTest.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(UnitTest.App_Start.NinjectWebCommon), "Stop")]
 
-namespace WebUl.App_Start
+namespace UnitTest.App_Start
 {
     using System;
     using System.Web;
