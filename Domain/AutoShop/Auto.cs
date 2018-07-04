8840e342c6ad8ed24995526fe25ba58959753b5b
Aleksey <alekseyarmany73@yandex.ru>
2018-07-04 07:00:06 +0400

    Создание класса модели данных

---
+++ Domain/AutoShop/Auto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создание класса модели данных
+namespace Domain.AutoShop
+{
+   public class Auto
+    {
+        public int AUTOSHOPId { get; set; }
+        public string NAME { get; set; }
+        public string ADRESS { get; set; }
+        public string CITY { get; set; }
+        public string TELEPHONE { get; set; }
+        public string SITE { get; set; }
+    }
+}
