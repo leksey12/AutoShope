fdc2f15226c06957faeb2bd1622528aa04f61ea0
Aleksey <alekseyarmany73@yandex.ru>
2018-07-04 06:54:46 +0400

    Создал в папке App_Start класс NinjectWebCommon.cs

---
+++ WebUl/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,36 @@
+﻿namespace WebUI.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon
+    {
+        
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+        private static void RegisterServices(IKernel kernel)
+        {
+            System.Web.Mvc.DependencyResolver.SetResolver(new WebUI.Infrastructure.NinjectDependencyResolver(kernel));
+        }
+    }
+}
