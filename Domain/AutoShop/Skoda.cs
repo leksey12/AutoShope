8efe4433f20eb741c3db19f47a3564fea7ecda23
Aleksey <alekseyarmany73@yandex.ru>
2018-07-06 07:39:49 +0400

    (2) класс, контроллер и преставление.

---
+++ Domain/AutoShop/Skoda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.AutoShop;
+
+namespace Domain.AutoShop
+{
+    [Table("Skoda")]
+    public  class Skoda
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string AutosID { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebUl/Controllers/SkodaController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUl.Controllers
+{
+    public class SkodaController : Controller
+    {
+        // конструктор, который объявляет зависимость от интерфейса IAutoRepository
+        private ISkodaRepository repository;
+        public SkodaController(ISkodaRepository repo)
+        {
+            repository = repo;
+        }
+        // метод действия List(), который создает представление
+        public ViewResult List()
+        {
+            return View(repository.AutoSkoda);
+        }
+    }
+}
