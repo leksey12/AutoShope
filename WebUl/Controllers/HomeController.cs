95879cc0561f23582930a4b6d09750102612f440
Aleksey <alekseyarmany73@yandex.ru>
2018-07-07 07:15:29 +0400

    Создал контроллер для главной станицы сайта

---
+++ WebUl/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUl.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly EFDbContext _db = new EFDbContext();
+        // GET: Home
+        public ViewResult Skodas()
+        {
+            var Skodas = _db.Skodas.ToList();
+            ViewBag.Skodas = Skodas;
+            return View();
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
